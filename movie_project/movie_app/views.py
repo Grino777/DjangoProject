c58caa15a8089c22f73a4154e9cb9cc72b2d5695
Grino777 <bars_B_P@mail.ru>
2023-03-13 19:05:33 +0300

    Update models.py

--- movie_project/movie_app/views.py
+++ movie_project/movie_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.db import connection
+from models import Movie
 
 # Create your views here.
 
 def index(request):
     context = {
-        'data': 'Ничего',
+        'data': connection.queries,
+        'movies': Movie.objects.all(),
     }
     return render(request, 'movie_app/index.html', context=context)
