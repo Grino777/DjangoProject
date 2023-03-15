61943a22d96318b40e9578556ac14a66739623b8
Grino777 <bars_B_P@mail.ru>
2023-03-15 19:36:17 +0300

    Added templates, css, update models.py. Added aggregate func.

--- movie_project/movie_app/urls.py
+++ movie_project/movie_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.show_all_movie),
-    path('movie/<int:id_movie>', views.get_movie_info, name='movie_detail'),
+    path('movie/<slug:slug_movie>', views.get_movie_info, name='movie_detail'),
 ]
--- movie_project/movie_app/views.py
+++ movie_project/movie_app/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Movie
+from django.db.models import F, Avg, Max, Min, Sum, Count
 
 def show_all_movie(request):
     movies = Movie.objects.all()
+    agg = movies.aggregate(Count('id'),
+                           Avg('budget'),
+                           Max('rating'),
+                           Min('rating'),
+                           )
     context = {
         'movies': movies,
+        'agg': agg,
     }
     return render(request, 'movie_app/show_all_movie.html', context=context)
 
-def get_movie_info(request, id_movie: int):
-    movie = get_object_or_404(Movie, id=id_movie)
+def get_movie_info(request, slug_movie: str):
+    movie = get_object_or_404(Movie, slug=slug_movie)
     context = {
         'movie': movie,
     }
