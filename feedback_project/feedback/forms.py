ef05e8c7447419555b0d3dac64a38506582d4b55
Grino777 <bars_B_P@mail.ru>
2023-03-22 15:04:50 +0300

    Update project

--- feedback_project/feedback/forms.py
+++ feedback_project/feedback/forms.py
@@ -1,11 +1,43 @@
 from django import forms
 
-class FeedbackForm(forms.Form):
-    name = forms.CharField(label='Имя', max_length=7, min_length=2, error_messages={
-        'max_lenght': 'Слишком много символов, должно быть %(limit_value)d, сейчас символов %(show_value)d.',
-        'min_length': 'Слишком мало символов, должно быть %(limit_value)d, сейчас символов %(show_value)d.',
-        'required': 'Поле не заполнено',
-    })
-    surname = forms.CharField(label='Фамилия')
-    feedback = forms.CharField(widget=forms.Textarea(attrs={"cols": "30", "rows": "10"}), label='Отзыв')
-    rating = forms.IntegerField(label='Рейтинг', min_value=1, max_value=5)
+from .models import Feedback
+
+
+# class FeedbackForm(forms.Form):
+#     name = forms.CharField(label='Имя', max_length=7, min_length=2, error_messages={
+#         'max_lenght': 'Слишком много символов, должно быть %(limit_value)d, сейчас символов %(show_value)d.',
+#         'min_length': 'Слишком мало символов, должно быть %(limit_value)d, сейчас символов %(show_value)d.',
+#         'required': 'Поле не заполнено',
+#     })
+#     surname = forms.CharField(label='Фамилия')
+#     feedback = forms.CharField(widget=forms.Textarea(attrs={"cols": "20", "rows": "2"}), label='Отзыв')
+#     rating = forms.IntegerField(label='Рейтинг', min_value=1, max_value=5)
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        # fields = ['name', 'surname', 'feedback', 'rating']
+        fields = '__all__'
+        labels = {
+            'name': 'Имя',
+            'surname': 'Фамилия',
+            'feedback': 'Отзыв',
+            'rating': 'Рейтиг',
+        }
+        error_messages = {
+            'name': {
+                'max_lenght': 'Слишком много символов, должно быть %(limit_value)d, сейчас символов %(show_value)d.',
+                'min_length': 'Слишком мало символов, должно быть %(limit_value)d, сейчас символов %(show_value)d.',
+                'required': 'Поле не заполнено',
+            },
+            'surname': {
+                'max_lenght': 'Слишком много символов, должно быть %(limit_value)d, сейчас символов %(show_value)d.',
+                'min_length': 'Слишком мало символов, должно быть %(limit_value)d, сейчас символов %(show_value)d.',
+                'required': 'Поле не заполнено',
+            },
+            'rating': {
+                'max_lenght': 'Рейтинг должен быть от 1 до 5',
+                'min_length': 'Рейтинг должен быть от 1 до 5',
+                'required': 'Поле не заполнено',
+            }
+        }
--- feedback_project/feedback/models.py
+++ feedback_project/feedback/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator, MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=75, validators=[MaxLengthValidator(75)])
+    surname = models.CharField(max_length=75, validators=[MaxLengthValidator(75)])
+    feedback = models.TextField()
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
