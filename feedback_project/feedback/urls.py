5893ff59c061d59aba36b7896fa74c52821bedef
Grino777 <bars_B_P@mail.ru>
2023-03-25 16:07:01 +0300

    Update project

--- feedback_project/feedback/urls.py
+++ feedback_project/feedback/urls.py
@@ -4,9 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.FeedbackView.as_view()),
-    path('<int:id_feedback>', views.UpdateFeedbackView.as_view()),
     path('done', views.DoneView.as_view()),
-    path('feedback/<int:pk>', views.DetailFeedback.as_view()),
-    path('update/<int:pk>', views.FeedbackViewUpdate.as_view()),
+    path('feedback/<int:pk>', views.DetailFeedback.as_view(), name='feedback_info'),
+    path('update/<int:pk>', views.FeedbackUpdateView.as_view(), name='update_feedback'),
     path('feedbacks', views.ListFeedbackView.as_view()),
 ]
--- feedback_project/feedback/views.py
+++ feedback_project/feedback/views.py
@@ -2,26 +2,26 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView, TemplateView, ListView, FormView, CreateView, UpdateView
 from .forms import FeedbackForm
 from .models import Feedback
-
+from django.urls import reverse
 from django.views import View
 
 
 # Create your views here.
 
-class FeedbackViewUpdate(UpdateView):
+class FeedbackUpdateView(UpdateView):
     model = Feedback
     form_class = FeedbackForm
     template_name = 'feedback/index.html'
-    success_url = '/done'
+    success_url = '/feedbacks'
+
 
 class FeedbackView(CreateView):
     '''Класс для отображени/сохранения данных формы с указанием БД.
     !Можно не указывать form_class, тогда форма построится на основании модели, НО есть ограничения по настройке полей.'''
-    model = Feedback #Куда будут сохранятся данные с формы
-    form_class = FeedbackForm #Перечисление полей формы
+    model = Feedback  # Куда будут сохраняться данные с формы
+    form_class = FeedbackForm  # Перечисление полей формы
     template_name = 'feedback/index.html'
     success_url = '/done'
-
 
 
 # class FeedbackView(FormView):
@@ -74,24 +74,25 @@ class FeedbackView(CreateView):
 class DoneView(TemplateView):
     template_name = 'feedback/done.html'
 
+
 # def done(request):
 #     return render(request, 'feedback/done.html')
 
-class UpdateFeedbackView(View):
-    def get(self, request, id_feedback):
-        feed = get_object_or_404(Feedback, id=id_feedback)
-        form = FeedbackForm(instance=feed)
-        return render(request, 'feedback/index.html', context={'form': form})
-
-    def post(self, request, id_feedback):
-        feed = get_object_or_404(Feedback, id=id_feedback)
-        form = FeedbackForm(request.POST, instance=feed)
-        if form.is_valid():
-            print(form.cleaned_data)
-            form.save()
-        else:
-            form = FeedbackForm(request.POST, instance=feed)
-        return render(request, 'feedback/index.html', context={'form': form})
+# class UpdateFeedbackView(View):
+#     def get(self, request, id_feedback):
+#         feed = get_object_or_404(Feedback, id=id_feedback)
+#         form = FeedbackForm(instance=feed)
+#         return render(request, 'feedback/index.html', context={'form': form})
+#
+#     def post(self, request, id_feedback):
+#         feed = get_object_or_404(Feedback, id=id_feedback)
+#         form = FeedbackForm(request.POST, instance=feed)
+#         if form.is_valid():
+#             print(form.cleaned_data)
+#             form.save()
+#         else:
+#             form = FeedbackForm(request.POST, instance=feed)
+#         return render(request, 'feedback/index.html', context={'form': form})
 
 # def update_feedback(request, id_feedback):
 #     feed = Feedback.objects.get(id=id_feedback)
@@ -119,8 +120,6 @@ class DetailFeedback(DetailView):
     context_object_name = 'feedback'
 
 
-
-
 class ListFeedbackView(ListView):
     template_name = 'feedback/feedbacks.html'
     model = Feedback
@@ -130,4 +129,3 @@ class ListFeedbackView(ListView):
         queryset = super().get_queryset()
         queryset = queryset.order_by('-rating')
         return queryset
-
